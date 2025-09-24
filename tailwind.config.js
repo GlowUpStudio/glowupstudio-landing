fe6cad1aa9e773af8f89a601a2b92c6091f92c05
VaneDev94 <vanedev.contacto@gmail.com>
2025-09-24 19:48:36 +0200

    cosis

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,16 @@ export default {
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"]
-      }
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
